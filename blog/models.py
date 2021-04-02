7a681b8ec63501c908635a5acec2527da02d57cf
Javier Espinoza <jeer2234@gmail.com>
2021-04-02 15:32:23 -0400

    implementacion de sistema de compartir post por email y comentarios en los post

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 class PublishedManager(models.Manager):
 
     def get_queryset(self):
@@ -36,3 +35,19 @@ class Post(models.Model):
     def __str__(self):
         
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+ 
+        return f'Comment by {self.name} on {self.post}'
--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nelsonloreto21@gmail.com'
+EMAIL_HOST_PASSWORD = 'genesis0505'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'

b93f8605f766394c912be1a78696c1cd22f0f185
Javier Espinoza <jeer2234@gmail.com>
2021-04-04 13:38:47 -0400

    implementacion de un sitemap

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'blog'
+
+
 
 urlpatterns = [
     # post views
@@ -10,6 +13,5 @@ urlpatterns = [
     #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
     path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
-    path('<int:post_id>/share/',
- views.post_share, name='post_share'),
+    path('<int:post_id>/share/',views.post_share, name='post_share'),
  ]

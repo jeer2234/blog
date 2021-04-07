fd9a67d31cdeacfabb4e11f28481faf7f152cf7d
Javier Espinoza <jeer2234@gmail.com>
2021-04-07 12:46:07 -0400

    motor de busqueda basico mas template de busqueda

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+from django.contrib.postgres.search import SearchVector
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 
@@ -94,3 +95,17 @@ def post_detail(request, year, month, day, post):
     'new_comment': new_comment,
     'comment_form': comment_form,
     'similar_posts': similar_posts})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+            search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request,'blog/post/search.html',
+    {'form': form,'query': query,'results': results})
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 
     # my apps
       
@@ -87,9 +88,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '123456',
+        }
 }
 
 
